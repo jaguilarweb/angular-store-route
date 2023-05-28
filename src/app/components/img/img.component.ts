295a4fe28db095ddd68191b9536a997794cb3b24
Jenny Aguilar <contact@jaguilarweb.com>
2023-05-28 13:53:47 +0200

    feat: Add output feature to send data from img child to app parent

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,11 +6,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./img.component.scss']
 })
 export class ImgComponent implements OnInit {
+
   @Input() img: string= 'valor inicial';
+  @Output() loaded = new EventEmitter<string>();
+  imgDefault: string = './assets/images/default.png';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  imgError(){
+    this.img = this.imgDefault;
+  }
+
+  imgLoaded(){
+    console.log('Log Hijo');
+    //Como en EventEmitter<string>() tipeamos una string
+    //En el emit, enviamos una string
+    this.loaded.emit(this.img);
+  }
 }
