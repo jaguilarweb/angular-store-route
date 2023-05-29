040be8216c1b194f09781c4c559bbcf50aa73d44
Jenny Aguilar <contact@jaguilarweb.com>
2023-05-29 15:30:11 +0200

    feat: Add shoppingCart methods

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -8,11 +8,13 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  myShoppingCart: Product[]=[];
+  total: number = 0;
   products: Product[] = [
     { id: '1', name: 'Product Name 1', image: './assets/images/toy.jpeg', price: 100 },
-    { id: '2', name: 'Product Name 2', image: './assets/images/toy.jpeg', price: 100 },
-    { id: '3', name: 'Product Name 3', image: './assets/images/toy.jpeg', price: 100 },
-    { id: '4', name: 'Product Name 4', image: './assets/images/toy.jpeg', price: 100 },
+    { id: '2', name: 'Product Name 2', image: './assets/images/bike.jpeg', price: 100 },
+    { id: '3', name: 'Product Name 3', image: './assets/images/books.jpeg', price: 100 },
+    { id: '4', name: 'Product Name 4', image: './assets/images/glasses.jpeg', price: 100 },
   ];
 
   constructor() { }
@@ -20,4 +22,9 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onAddToShoppingCart(product: Product) {
+    console.log(product);
+    this.myShoppingCart.push(product);
+    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0)
+  }
 }
