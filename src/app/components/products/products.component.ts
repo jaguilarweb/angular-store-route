7d90a96cda9e36db3ac837095b870e451e77f5c3
Jenny Aguilar <contact@jaguilarweb.com>
2023-05-30 11:49:32 +0200

    feat: Add products service

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,34 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from 'src/app/models/product.model';
 import { StoreService } from 'src/app/services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
   myShoppingCart: Product[] = [];
   total: number = 0;
-  products: Product[] = [
-    { id: '1', name: 'Product Name 1', image: './assets/images/toy.jpeg', price: 100 },
-    { id: '2', name: 'Product Name 2', image: './assets/images/bike.jpeg', price: 100 },
-    { id: '3', name: 'Product Name 3', image: './assets/images/books.jpeg', price: 100 },
-    { id: '4', name: 'Product Name 4', image: './assets/images/house.jpeg', price: 100 },
-  ];
+  products: Product[] = [];
 
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private productService: ProductsService
   ) {
     //Puede ir acá porque no es es async.
     this.myShoppingCart = this.storeService.getShoppingCart();
-   }
+  }
 
   ngOnInit(): void {
+    //Petición async
+    this.productService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    });
   }
 
   onAddToShoppingCart(product: Product) {
-    this.storeService.addProduct(product)
+    this.storeService.addProduct(product);
     this.total = this.storeService.getTotal();
   }
 }
