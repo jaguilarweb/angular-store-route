f8562c27eccd598c8ac3b049b5f426d038660495
Jenny Aguilar <contact@jaguilarweb.com>
2023-06-10 10:19:23 +0200

    refactor: Change to add switchMap in loginAndGet method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { switchMap } from 'rxjs';
 import { environment } from './../../environments/environment';
 import { Auth } from '../models/auth.model';
 import { User } from '../models/user.model';
@@ -17,12 +17,19 @@ export class AuthService {
     return this.http.post<Auth>(`${this.apiUrl}/login`, { email, password });
   }
 
-  profile(token: string) {
+  getProfile(token: string) {
     return this.http.get<User>(`${this.apiUrl}/profile`, {
       headers: {
         Authorization: `Bearer ${token}`,
         // 'Content-type': 'application/json' //optativo
       },
     });
+  }
+
+  loginAndGet(email: string, password: string) {
+    return this.login(email, password)
+    .pipe(
+      switchMap(rta => this.getProfile(rta.access_token)),
+    )
   }
 }
