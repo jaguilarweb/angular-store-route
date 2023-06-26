7c9ec2daefe8698f6b33775199cad0041676ccc3
Jenny Aguilar <contact@jaguilarweb.com>
2023-06-26 20:36:17 +0200

    feat: Create exit guard to not allow navigate from the guard

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -11,6 +11,7 @@ import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { AuthGuard } from '../guards/auth.guard';
+import { ExitGuard } from '../guards/exit.guard';
 
 const routes: Routes = [
   {
@@ -30,7 +31,7 @@ const routes: Routes = [
       { path: 'product/:id', component: ProductDetailComponent },
       { path: 'my-cart', component: MyCartComponent },
       { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
+      { path: 'register', canDeactivate: [ExitGuard], component: RegisterComponent },
       { path: 'recovery', component: RecoveryComponent },
       {
         path: 'profile',
